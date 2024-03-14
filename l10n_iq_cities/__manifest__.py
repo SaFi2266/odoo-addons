d8c1777f03718f17f5f478e0f4c6cf45c8813f14
Mustafa SAADI <safi2266@gmail.com>
2024-03-14 19:26:17 +0300

    mytest

---
+++ l10n_iq_cities/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Iraq Localization",
+    "version": "17.0.0.1",
+    "category": "Localizations",
+    'summary': "Set Iraqi states and cities",
+    "author": "Mustafa SAADI, IQSYS",
+    "website" : "https://imerps.com",
+    "auto_install": False,
+    "depends": ["base_address_extended"],
+    'installable': True,
+    'application': False,
+    'data': [
+        'data/res.country.state.csv',
+        'data/res.city.csv',
+        'data/res_country_data.xml',
+        'views/res_partner_views.xml',
+    ],
+    'license': 'LGPL-3',
+    'images': ['static/description/banner.png'],
+}
