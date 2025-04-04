7c2e8845f6d89b41f9e38e8fc185e2474ed88823
Mustafa SAADI <5728273+SaFi2266@users.noreply.github.com>
2025-04-04 13:20:30 +0300

    Update res_partner.py
    State name translated

--- l10n_iq_cities/models/res_partner.py
+++ l10n_iq_cities/models/res_partner.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+class CountryState(models.Model):
+    _inherit = "res.country.state"
+
+    name = fields.Char(translate=True)
 
 class Partner(models.Model):
     _inherit = 'res.partner'
